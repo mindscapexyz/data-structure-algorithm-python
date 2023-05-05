7e291ec2b55d0bdcdd48b6ccf0ea7c462b91a9f9
Shukor, Halim <shukor.h@duerr-ndt.com>
2023-05-05 09:47:33 +0800

    add "add" method

--- fraction.py
+++ fraction.py
@@ -8,6 +8,14 @@ class Fraction:
     def __str__(self):
         return str(self.num) + "/" + str(self.den)
 
+    def __add__(self, other_fraction):
+        new_num = self.num * other_fraction.den + self.den * other_fraction.num
+        new_den = self.den * other_fraction.den
+        return Fraction(new_num, new_den)
+
 
 my_f = Fraction(3, 5)
 print(my_f)
+my_f_s = Fraction(1, 5)
+sum_f = my_f + my_f_s
+print(sum_f)
