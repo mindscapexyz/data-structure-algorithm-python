57905e4666fa83cad4cdc32dcbeb013a6747118e
Shukor, Halim <shukor.h@duerr-ndt.com>
2023-05-11 18:23:57 +0800

    simple parity checker

---
+++ data-structure/Stack/simple_balance_paratheses.py
@@ -0,0 +1,27 @@
+import stack
+
+
+def par_checker(symbol_string):
+    s = stack.Stack()
+    balanced = True
+    index = 0
+    while index < len(symbol_string) and balanced:
+        symbol = symbol_string[index]
+        if symbol == "(":
+            s.push(symbol)
+        else:
+            if s.is_empty():
+                balanced = False
+            else:
+                s.pop()
+
+        index = index + 1
+
+    if balanced and s.is_empty():
+        return True
+    else:
+        return False
+
+
+print(par_checker("((()))"))
+print(par_checker("(()"))
--- data-structure/Stack/stack.py
+++ data-structure/Stack/stack.py
@@ -5,19 +5,14 @@ class Stack:
     def is_empty(self):
         return self.items == []
 
+    def push(self, item):
+        return self.items.append(item)
 
-def push(self, item):
-    return self.items.append(item)
+    def pop(self):
+        return self.items.pop()
 
+    def peek(self):
+        return self.items[len(self.items) - 1]
 
-def pop(self):
-    return self.items.pop()
-
-
-def peek(self):
-    return self.items[len(self.items) - 1]
-
-
-def size(self):
-    return len(self.items)
-
+    def size(self):
+        return len(self.items)
